40da3f42e6b6a05071187ae4b9f8de908f7424c1
armanisadeghi <132974515+armanisadeghi@users.noreply.github.com>
2024-08-16 21:41:49 -0700

    more files pushed

---
+++ lib/fonts.ts
@@ -0,0 +1,14 @@
+// lib/fonts.ts
+import { Inter, Montserrat } from 'next/font/google'
+
+export const inter = Inter({
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-inter',
+})
+
+export const montserrat = Montserrat({
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-montserrat',
+})
